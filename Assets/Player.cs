1aeab7e5c0173ae8079ec31cfb88e6fca564960c
Jacob Aaron Sabado <jacobaaron.sabado@benilde.edu.ph>
2025-01-24 18:04:04 +0800

    Added Simple Spacebar control for Expanding Bubble
    Holding Spacebar expands bubble. Releasing it shrinks it.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float scaleFactor = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space) == true)
+        {
+            transform.localScale += Vector3.one * scaleFactor;
+        }
+        else if (Input.GetKey(KeyCode.Space) == false)
+        {
+            transform.localScale -= Vector3.one * scaleFactor;
+            //if (transform.localScale != Vector3.one) transform.localScale = Vector3.one;
+        }
         
     }
 }
