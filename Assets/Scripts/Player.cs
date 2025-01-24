61c2d965258c9f0f9e2b6e02daceed919228e076
Jacob Aaron Sabado <jacobaaron.sabado@benilde.edu.ph>
2025-01-24 23:28:46 +0800

    Added camera and Floating mechanic to the player.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float scaleFactor = 0.01f;
-    [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private float scaleFactor = 0.001f;
+    [SerializeField] private float moveSpeed = 3.0f;
     [SerializeField] private float bubbleLimit = 3.0f;
     [SerializeField] private float bubbleTimer;
+    private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,18 +24,27 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             transform.localScale += Vector3.one * scaleFactor;
+            rb2d.gravityScale = -1.0f;
+            rb2d.gravityScale += -0.05f;
         }
         else if (Input.GetKey(KeyCode.Space) == false)
         {
             transform.localScale -= Vector3.one * scaleFactor;
+            rb2d.gravityScale = 1.0f;
+
+            bubbleTimer = 0;
+
             if (transform.localScale.x < 1) transform.localScale = Vector3.one;
         }
 
-        if (transform.localScale.x >= bubbleLimit) transform.localScale = Vector3.one * bubbleLimit;
+        if (transform.localScale.x >= bubbleLimit) 
+        {
+            transform.localScale = Vector3.one * bubbleLimit;
+        }
 
         if (transform.localScale.x == bubbleLimit)
         {
-            if (bubbleTimer >= 150)
+            if (bubbleTimer >= 300)
             {
                 Destroy(this.gameObject);
             }
