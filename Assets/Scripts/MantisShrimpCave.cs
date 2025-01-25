b81d5accb1b8ea00c1af218c2bdab6550f4ccaf7
Jacob Aaron Sabado <jacobaaron.sabado@benilde.edu.ph>
2025-01-25 23:01:22 +0800

    Added the Mantis Shrimp cave (assets pending)

---
+++ Assets/Scripts/MantisShrimpCave.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MantisShrimpCave : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("You gon die buddy");
+            StartCoroutine(ImminentMantisDeath(collision.gameObject));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("You live buddy");
+            StopCoroutine(ImminentMantisDeath(collision.gameObject));
+        }
+    }
+
+    IEnumerator ImminentMantisDeath(GameObject deadPlayer)
+    {
+        // Wait for the specified delay
+        yield return new WaitForSeconds(2.0f);
+
+        // Disable the player GameObject
+        deadPlayer.SetActive(false);
+    }
+}
